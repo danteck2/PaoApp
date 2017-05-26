2842f7e5e8fc3aa6c6f3d973867d66f7accfa8ed
Dan Odobescu <dan.odobescu@gmail.com>
2017-05-26 15:37:11 +0300

    Final Version

--- PaoApp/src/paopackage/Calculator.java
+++ PaoApp/src/paopackage/Calculator.java
@@ -4,112 +4,73 @@ import javax.swing.*;
 import java.awt.GridLayout;
 import java.awt.BorderLayout;
 
-public class Calculator {
+public class Calculator{
 
 	JPanel windowContent;
-	private JTextField displayField;
-	JButton button0;
-	JButton button1;
-	JButton button2;
-	JButton button3;
-	JButton button4;
-	JButton button5;
-	JButton button6;
-	JButton button7;
-	JButton button8;
-	JButton button9;
-	JButton buttonPoint;
-	JButton buttonEqual;
-	JButton buttonPlus;
-	JButton buttonMinus;
-	JButton buttonMult;
-	JButton buttonDiv;
+	private JLabel displayString;
+	JButton buttons[];
 	JPanel p1;
 
-    public void setDisplayValue(String val) {
-        displayField.setText(val);
+    public void setDisplayString(String s){
+    	displayString.setText(s);
+	}
+    public String getDisplayString(){
+        return displayString.getText();
     }
-    
-    public String getDisplayValue() {
-        return displayField.getText();
-    }
-    
-    Calculator() {
+    Calculator(){
     	//JPanel
     	windowContent = new JPanel();
     	BorderLayout bl = new BorderLayout(); 
     	windowContent.setLayout(bl);
-    	displayField = new JTextField(30);
-    	windowContent.add("North",displayField);
-
-		button0 = new JButton("0");
-		button1 = new JButton("1");
-		button2 = new JButton("2");
-		button3 = new JButton("3");
-		button4 = new JButton("4");
-		button5 = new JButton("5");
-		button6 = new JButton("6");
-		button7 = new JButton("7");
-		button8 = new JButton("8");
-		button9 = new JButton("9");
-		buttonPoint = new JButton(".");
-		buttonEqual = new JButton("=");
-		buttonPlus = new JButton("+");
-		buttonMinus = new JButton("-");
-		buttonMult = new JButton("*");
-		buttonDiv = new JButton("/");
+    	displayString = new JLabel("0");
+    	windowContent.add("North",displayString);
+    	
+    	buttons = new JButton[16];
+    	for (int i=0; i<=9; i++){
+    		buttons[i] = new JButton(String.valueOf(i));
+    	}
+    	buttons[10] = new JButton(".");
+		buttons[11] = new JButton("=");
+		buttons[12] = new JButton("+");
+		buttons[13] = new JButton("-");
+		buttons[14] = new JButton("*");
+		buttons[15] = new JButton("/");
 
 		p1 = new JPanel();
 		GridLayout gl = new GridLayout(4,4); 
 		p1.setLayout(gl);
 
-		//Add buttons
-		p1.add(button7);
-		p1.add(button8);
-		p1.add(button9);
-		p1.add(buttonMult);
-		p1.add(button4);
-		p1.add(button5);
-		p1.add(button6);
-		p1.add(buttonMinus);
-		p1.add(button1);
-		p1.add(button2);
-		p1.add(button3);
-		p1.add(buttonPlus);
-		p1.add(buttonDiv);
-		p1.add(button0);
-		p1.add(buttonPoint);
-		p1.add(buttonEqual);
+		p1.add(buttons[7]);
+		p1.add(buttons[8]);
+		p1.add(buttons[9]);
+		p1.add(buttons[14]);
+		p1.add(buttons[4]);
+		p1.add(buttons[5]);
+		p1.add(buttons[6]);
+		p1.add(buttons[13]);
+		p1.add(buttons[1]);
+		p1.add(buttons[2]);
+		p1.add(buttons[3]);
+		p1.add(buttons[12]);
+		p1.add(buttons[15]);
+		p1.add(buttons[0]);
+		p1.add(buttons[10]);
+		p1.add(buttons[11]);
 		
 		CalculatorEngine calcEngine = new CalculatorEngine(this);
-		button0.addActionListener(calcEngine);
-		button1.addActionListener(calcEngine);
-		button2.addActionListener(calcEngine);
-		button3.addActionListener(calcEngine);
-		button4.addActionListener(calcEngine);
-		button5.addActionListener(calcEngine);
-		button6.addActionListener(calcEngine);
-		button7.addActionListener(calcEngine);
-		button8.addActionListener(calcEngine);
-		button9.addActionListener(calcEngine);
-		buttonPoint.addActionListener(calcEngine);
-		buttonEqual.addActionListener(calcEngine);
-		buttonPlus.addActionListener(calcEngine);
-		buttonMinus.addActionListener(calcEngine);
-		buttonMult.addActionListener(calcEngine);
-		buttonDiv.addActionListener(calcEngine);
-			          
+		for (int i=0; i<buttons.length; i++){
+			buttons[i].addActionListener(calcEngine);
+		}			          
 		windowContent.add("Center",p1);
         
 		JFrame frame = new JFrame("Calculator");
 		frame.setContentPane(windowContent);
 		//Seteaza automat dimensiunea 
 		frame.pack();
-		
 		frame.setVisible(true);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
     	new Calculator();
     }
 }
--- PaoApp/src/paopackage/CalculatorEngine.java
+++ PaoApp/src/paopackage/CalculatorEngine.java
@@ -4,32 +4,146 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JButton;
 
-public class CalculatorEngine implements ActionListener {
- 
+public class CalculatorEngine implements ActionListener{
+	
 	Calculator parent;
-
+	boolean display;
+	boolean clear;
+	double lastNumber;
+	String lastOperator;
+	
 	CalculatorEngine(Calculator parent){
 		this.parent = parent;
+		parent.setDisplayString("0");
+		lastOperator = "0";
+		lastNumber = 0;
+		display = false;
+		clear = true;
 	}
-
 	public void actionPerformed(ActionEvent e){
-		
-		
-		
-		
-		
-		
-		
-   JButton clickedButton =  (JButton) e.getSource();
-   	
-   // Get the existing text from the CalculatorÕs
-   // display field. Reaching inside another object is bad.
-   String dispFieldText = parent.getDisplayValue();
+		JButton button =  (JButton) e.getSource();
+		String buttonText = button.getText();
+		switch(buttonText){
+			case "0":
+				addDigit(0);
+				break;
+			case "1":
+				addDigit(1);
+				break;
+			case "2":
+				addDigit(2);
+				break;
+			case "3":
+				addDigit(3);
+				break;
+			case "4":
+				addDigit(4);
+				break;
+			case "5":
+				addDigit(5);
+				break;
+			case "6":
+				addDigit(6);
+				break;
+			case "7":
+				addDigit(7);
+				break;
+			case "8":
+				addDigit(8);
+				break;
+			case "9":
+				addDigit(9);
+				break;
+			case ".":
+				addPoint();
+				break;
+			case "=":
+				processEqual();
+				break;
+			case "+":
+				processOperator("+");
+				break;
+			case "-":
+				processOperator("-");
+				break;
+			case "*":
+				processOperator("*");
+				break;
+			case "/":
+				processOperator("/");
+				break;
+		}
+	}
+	void addDigit(int x){
+		if (clear)
+			parent.setDisplayString("");
 
-   // Get the button's label 
-   String clickedButtonLabel = clickedButton.getText();
-   	
-   parent.setDisplayValue(dispFieldText + 
-                                   clickedButtonLabel);
+		String input = parent.getDisplayString();
+		
+		if (input.indexOf("0") == 0)
+			input = input.substring(1);
+		if ((!input.equals("0") || x > 0)  && input.length() < 20)
+			parent.setDisplayString(input + x);
+
+		display = false;
+		clear = false;
+	}
+	void addPoint(){
+		display = false;
+
+		if (clear)
+			parent.setDisplayString("");
+
+		String input = parent.getDisplayString();
+		
+		if (input.indexOf(".") < 0)
+			parent.setDisplayString(new String(input + "."));
+	}
+	
+	double getNumber(){
+		String input = parent.getDisplayString();
+		return Double.parseDouble(input);
+	}
+	
+	void processOperator(String op) {
+		if (display == false || display == true){
+			double number = getNumber();
+
+			if (!lastOperator.equals("0")) {
+				double result = processLastOperator();
+				displayResult(result);
+				lastNumber = result;
+			}
+			else lastNumber = number;
+			
+			clear = true;
+			lastOperator = op;
+		}
+	}
+	void processEqual(){
+		double result = 0;
+		result = processLastOperator();
+		displayResult(result);
+		lastOperator = "0";
+	}
+	double processLastOperator(){
+		double result = 0;
+		double number = getNumber();
+	
+		if (lastOperator.equals("/"))
+			result = lastNumber / number;
+		if (lastOperator.equals("*"))
+			result = lastNumber * number;
+		if (lastOperator.equals("-"))
+			result = lastNumber - number;
+		if (lastOperator.equals("+"))
+			result = lastNumber + number;
+		return result;
+	}
+	void displayResult(double result){
+		parent.setDisplayString(Double.toString(result));
+		lastNumber = result;
+		display = true;
+		clear = true;
 	}
 }
